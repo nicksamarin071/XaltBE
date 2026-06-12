fecaf3faf2ae26c092b5e1d76b80ed6420a7442e
mac <bibin@bellatorgroup.in>
2026-06-12 17:29:59 +0530

    deleted unwanted files

--- dist/controller/commonController.d.ts
+++
@@ -1,3 +0,0 @@
-import type { NextFunction, Request, Response } from "express";
-export declare const healthCheckC: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-//# sourceMappingURL=commonController.d.ts.map
