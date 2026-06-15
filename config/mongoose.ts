c6d712c3eea1a39f8e0532d792889fb2c1d8092a
mac <bibin@bellatorgroup.in>
2026-06-15 13:51:31 +0530

    Testing mongoose

--- config/mongoose.ts
+++ config/mongoose.ts
@@ -1,22 +1,29 @@
 import mongoose from "mongoose";
-import { MONGO_DB, MONGO_HOST, MONGO_PORT, NODE_ENV,MONGO_URI } from "../utils/constants.js";
 import { logError, logInfo } from "../utils/debug.js";
 
-// MongoDB connection URI
-const mongoURI =
-(NODE_ENV === "dev" && MONGO_HOST && MONGO_PORT)
-? `mongodb://${MONGO_HOST}:${MONGO_PORT}`
-: MONGO_URI;
-
-logError(`My process .env is ${MONGO_URI}`);
 // Connect to MongoDB
 const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoURI as string);
+  const nodeEnv = process.env.NODE_ENV;
+  const mongoHost = process.env.MONGO_HOST;
+  const mongoPort = process.env.MONGO_PORT;
+  const mongoUri = process.env.MONGO_URI;
 
+  const finalURI =
+    nodeEnv === "dev" && mongoHost && mongoPort
+      ? `mongodb://${mongoHost}:${mongoPort}`
+      : mongoUri;
+
+  logInfo(`Attempting connection with URI: ${finalURI}`);
+
+  try {
+    if (!finalURI) {
+      throw new Error("MONGO_URI is missing or undefined in environment variables!");
+    }
+
+    await mongoose.connect(finalURI);
     logInfo("== Connected to MongoDB ==");
   } catch (error: any) {
-    logError(`== MongoDB connection error ${error.message}`);
+    logError(`== MongoDB connection error: ${error.message}`);
   }
 };
 
